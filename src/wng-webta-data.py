cf704182e43d2d3446406ed440ad4a6ea6526f8c
Greg Barbieri <gfbarbieri@gmail.com>
2019-07-14 11:02:46 -0400

    renamed wrangle file.

--- src/wng-webta-data.py
+++ src/wng-webta-data.py
@@ -1,33 +1,73 @@
+import datetime
+import os
 import pandas as pd
-import sqlite3
 
-def setDateTime(row, sheet):
-    sheet_num = int(sheet[5:])
-    dow, md  = row['Date'].split(" ", 2)
-    month, day = md.split("/", 2)
+def setSkiprows(file_name, sheet_name):
+    '''
+    Return an integer to set skiprows argument for the read_excel function.
+    '''
+    if file_name == 'webta_20140713_20160625.xlsx':
+        skiprows = 1
+    elif file_name == 'webta_20160626_20171028.xlsx' and int(sheet_name[5:]) < 33:
+        skiprows = 1
+    else:
+        skiprows = 2
 
-    if sheet_num < 20:
-        year = 2018
-    elif sheet_num == 20:
-        if int(month) == 1:
-            year = 2019
+    return skiprows
+
+def createDateRange(file_name, sheet_name):
+    '''
+    Return a dataframe with all of the date ranges covered in the timesheet
+    being worked on. All timesheets cover a 14-day period, 10 business days.
+    '''
+    file_start_date = pd.Timestamp(file_name.split("_")[1])
+    elapsed = int(sheet_name[5:])
+
+    if file_name == "webta_20140713_20160625.xlsx":
+        if elapsed > 29:
+            elapsed -= 1
         else:
-            year = 2018
-    elif sheet_num > 20:
-        year = 2019
+            elapsed = elapsed
 
-    dt_in = '{}-{}-{} {}'.format(year, month, day, row['Time In'])
-    dt_out = '{}-{}-{} {}'.format(year, month, day, row['Time Out'])
+    start_day = (elapsed-1)*14
+    end_day = (elapsed)*14
 
-    ts_date = pd.Timestamp(dt_in).strftime('%Y-%m-%d')
-    ts_in = pd.Timestamp(dt_in).strftime('%Y-%m-%d %H:%M:%S')
-    ts_out = pd.Timestamp(dt_out).strftime('%Y-%m-%d %H:%M:%S')
+    date_range_start = file_start_date + datetime.timedelta(days=start_day)
+    date_range_end = file_start_date + datetime.timedelta(days=end_day)
 
-    return ts_date, ts_in, ts_out
+    time_range = []
 
-def wrangleData(df, sheet):
+    for i in range(start_day,end_day):
+        date = file_start_date + datetime.timedelta(days=i)
+        date_month = str(date.month).zfill(2)
+        date_day = str(date.day).zfill(2)
+        date_year = str(date.year)
+
+        time_range.append([date_month, date_day, date_year])
+
+    time_range_df = pd.DataFrame(time_range, columns=['month','day','year'])
+
+    return time_range_df
+
+def setDateTimeInOut(row):
+    '''
+    Return three values, a date formatted as "YYYY-MM-DD", and
+    two time values (clocked in and out) formatted as "HH:MM:SS".
+    '''
+    d_str = '{} {} {}'.format(row['month'], row['day'], row['year'])
+
+    d_ts = pd.Timestamp(d_str).strftime('%Y-%m-%d')
+    tin_ts = pd.Timestamp(row['Time In']).strftime('%H:%M:%S')
+    tout_ts = pd.Timestamp(row['Time Out']).strftime('%H:%M:%S')
+
+    return d_ts, tin_ts, tout_ts
+
+def wrangleData(time_sheet_df, time_range_df):
+    '''
+    Return dataframe of wrangled timesheet data.
+    '''
     #Handle missing values.
-    df.dropna(axis=0, how='all', inplace=True)
+    df = time_sheet_df.dropna(axis=0, how='all')
 
     df = df.loc[df['Transaction'].isna() == False]
 
@@ -36,78 +76,67 @@ def wrangleData(df, sheet):
     df = df.loc[(df['Date'].isna() == False) &
                 (df['Date'] != 'Date')]
 
-    #Handle datetime.
-    df[['Date','time_in','time_out']] = df.apply(setDateTime, axis=1, result_type='expand', args=(sheet,))
+    #Create month and day columns.
+    df['md']  = df['Date'].str.split(pat=" ", n=2, expand=True)[1]
+    df[['month', 'day']] = df['md'].str.split("/", 2, expand=True)
+    df['month'] = df['month'].str.zfill(2)
+    df['day'] = df['day'].str.zfill(2)
 
-    #Handle transactions.
-    df = df.loc[(df['Transaction'] == '01 - Regular Base Pay') |
-                (df['Transaction'] == '29 - Credit Hours Worked-Regular Time')]
+    #Create year column.
+    df = df.merge(right=time_range_df, on=['month','day'])
 
-    df['time_out_max'] = df.groupby(by=['Date'])['time_out'].transform(max)
+    #Create formatted date, time-in, and time-out columns.
+    df[['date','time-in','time-out']] = df.apply(setDateTimeInOut, axis=1, result_type='expand')
 
-    df = df.loc[(df['Transaction'] == '01 - Regular Base Pay')]
+    #Rename columns.
+    rename = {'Shift Total':'shift_total',
+              'Meal':'lunch',
+              'Daily Total':'daily_total',
+              'Transaction':'transaction'}
 
-    #Finalize columns and format
-    df_in = df[['time_in']].copy()
-    df_in.loc[:, 'entry_type'] = 'Arrival'
-    df_in.loc[:, 'description'] = ""
-    df_in.rename(columns={'time_in':'datetimes'}, inplace=True)
+    df.rename(columns=rename, inplace=True)
 
-    df_out = df[['time_out_max']].copy()
-    df_out.loc[:, 'entry_type'] = 'Departure'
-    df_out.loc[:, 'description'] = ""
-    df_out.rename(columns={'time_out_max':'datetimes'}, inplace=True)
+    #Subset columns.
+    df = df[['date','time-in','time-out','shift_total',
+             'lunch','daily_total','transaction']]
 
-    df_final = pd.concat([df_in, df_out], sort=False)
-    df_final.reset_index(drop=True, inplace=True)
-
-    return df_final
-
-def createDB(data, table_name):
-    '''
-    Write dataframes to database tables.
-    '''
-    #Open connection and create cursor.
-    conn = sqlite3.connect('../data/arrival_departure_hist.db')
-    c = conn.cursor()
-
-    #If table already exists, drop it before writing to it.
-    c.execute("drop table if exists {}".format(table_name))
-
-    #Write to the table.
-    data.to_sql(table_name, conn)
-
-    #Commit and close connection.
-    conn.commit()
-    conn.close()
+    return df
 
 def main():
-    #Set file path.
-    file_name = "20180401_20190622.xlsx"
-    file_path = '../data/{}'.format(file_name)
-
-    #Determine total number of sheets.
-    xlsx = pd.ExcelFile(file_path)
-    sheets = xlsx.sheet_names
+    '''
+    Driver function.
+    '''
+    #Set folder path.
+    folder_path = "../data/time-data"
 
     #Define final pandas dataframe.
-    df_app = pd.DataFrame(columns=['datetimes','entry_type','description'])
+    df_app = pd.DataFrame(columns=['date','time-in','time-out','shift_total','lunch','daily_total','transaction'])
 
-    #Work over every sheet.
-    for sheet in sheets:
-        print(sheet)
+    #Work over every workbook and spreadsheet within the workbook.
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path,file)
 
-        #Import.
-        df_ing = pd.read_excel(xlsx, sheet_name=sheet, skiprows=2)
+        #Create excel reader and determine sheet names.
+        xlsx = pd.ExcelFile(file_path)
+        sheets = xlsx.sheet_names
 
-        #Wrangle.
-        df_wng = wrangleData(df_ing, sheet)
+        for sheet in sheets:
+            #Create time range dataframe from the file.
+            time_range_df = createDateRange(file_name=file, sheet_name=sheet)
 
-        #Append.
-        df_app = df_app.append(other=df_wng)
+            #Import timesheet.
+            print(file, sheet)
+            skiprows = setSkiprows(file_name=file, sheet_name=sheet)
+            df_ing = pd.read_excel(xlsx, sheet_name=sheet, skiprows=skiprows)
 
-    #Output.
-    createDB(data=df_app, table_name='arrive_depart_webta')
+            #Wrangle timesheet.
+            df_wng = wrangleData(time_sheet_df=df_ing, time_range_df=time_range_df)
+
+            #Append timesheets.
+            df_app = df_app.append(other=df_wng, ignore_index=True)
+
+    #Output timesheets to comma-separated file.
+    df_app.to_csv(path_or_buf='../data/timesheet.csv', sep=',', index=False)
 
 if __name__ == '__main__':
     main()
